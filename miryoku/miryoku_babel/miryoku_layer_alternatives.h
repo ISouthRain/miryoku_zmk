dafeab89f3156c8223e379a5927b6a613b919a0e
ISouthRain <isouthrain@gmail.com>
2025-03-23 12:11:16 +0800

    Add Layer: APPGAME

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -374,8 +374,8 @@ U_NA,              &kp RALT,          &u_to_U_MEDIA,     &u_to_U_FUN,       U_NA
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp TAB,           &kp SPACE,         &kp K_APP,         U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_FUN \
-&kp F12,           &kp F7,            &kp F8,            &kp F9,            &kp PSCRN,         &tog U_APPAD,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp F12,           &kp F7,            &kp F8,            &kp F9,            &kp PSCRN,         &tog U_APPAD,      &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,          &tog U_APPGAME,    &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp TAB,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
@@ -386,8 +386,15 @@ U_UND,             U_CUT,             U_CPY,             U_PST,             U_RD
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
+#define MIRYOKU_ALTERNATIVES_APPGAME \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
+&kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &tog U_APPGAME,    \
+U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
 #define MIRYOKU_ALTERNATIVES_APPAD \
-&kp INS,            &kp FSLH,           &kp UP,               &kp ASTERISK,            &kp MINUS,   &tog U_APPAD,   &kp KP_NUMBER_7,      &kp KP_NUMBER_8,     &kp KP_NUMBER_9,        &kp NON_US_BACKSLASH,            \
+&kp INS,            &kp FSLH,         &kp UP,               &kp ASTERISK,             &kp MINUS,   &tog U_APPAD,   &kp KP_NUMBER_7,      &kp KP_NUMBER_8,     &kp KP_NUMBER_9,        &kp NON_US_BACKSLASH,            \
 U_MT(LGUI, DEL),  U_MT(LALT, LEFT),   U_MT(LCTRL, DOWN),    U_MT(LSHFT, RIGHT),       &kp PLUS,     U_NA,          &kp KP_NUMBER_4,      &kp KP_NUMBER_5,     &kp KP_NUMBER_6,        &kp LGUI,          \
 &kp Z,               &kp X,           &kp C,                &kp V,                    &kp PERIOD,   U_NA,          &kp KP_NUMBER_1,      &kp KP_NUMBER_2,     &kp KP_NUMBER_3,        &kp PLUS,              \
 U_NP,                U_NP,            &kp ESC,              &kp SPACE,                &kp TAB,      U_NA,          &kp KP_NUMBER_0,             U_NA,              U_NP,           U_NP
+
